7a85c19c8f7433b8c5616d80e32f456f599a5fbe
Dilpat Rai <67273086+DilpatRai@users.noreply.github.com>
2025-03-19 11:42:09 +0500

    Update Dashboard.java

--- AchieveGPA/src/main/java/cs411/ui/Dashboard.java
+++ AchieveGPA/src/main/java/cs411/ui/Dashboard.java
@@ -69,7 +69,7 @@ public class Dashboard extends JFrame {
         contentPanel = new JPanel(cardLayout);
         contentPanel.setBackground(Config.PRIMARY_COLOR);
 
-       tabs.forEach((tab, index) -> {
+        tabs.forEach((tab, index) -> {
     switch (tab) {
         case "Courses" -> contentPanel.add(createCoursesTab(), tab);
         case "Students" -> contentPanel.add(createStudentsTab(), tab);
@@ -78,7 +78,7 @@ public class Dashboard extends JFrame {
         default -> System.out.println("Invalid tab: " + tab);
     }
 });
-add(contentPanel, BorderLayout.CENTER);
+        add(contentPanel, BorderLayout.CENTER);
 
 
         sidebarPanel.setSize(new Dimension(300, Integer.MAX_VALUE));
@@ -90,7 +90,7 @@ add(contentPanel, BorderLayout.CENTER);
         JButton profileButton = new JButton("Profile");
         profileButton.setAlignmentX(Component.CENTER_ALIGNMENT);
         profileButton.setMaximumSize(new Dimension(Integer.MAX_VALUE, 50));
-        profileButton.addActionListener(event -> openProfile());
+        profileButton.addActionListener(e -> openProfile());
         profileButton.setOpaque(false);
         profileButton.setContentAreaFilled(false);
         profileButton.setBorderPainted(true);
@@ -260,7 +260,7 @@ add(contentPanel, BorderLayout.CENTER);
         JButton updateButton = new JButton("Update");
         updateButton.setAlignmentX(Component.CENTER_ALIGNMENT);
         updateButton.setForeground(Config.PRIMARY_COLOR);
-        updateButton.addActionListener(_ -> {
+        updateButton.addActionListener(e -> {
             if (passwordField.getText().trim().length() < 8) {
                 JOptionPane.showMessageDialog(null, "Password must be at least 8 characters long");
                 return;
@@ -326,7 +326,7 @@ add(contentPanel, BorderLayout.CENTER);
         myCoursesButton.setMinimumSize(new Dimension(250, 40));
         myCoursesButton.setPreferredSize(new Dimension(250, 40));
         myCoursesButton.setMaximumSize(new Dimension(250, 40));
-        myCoursesButton.addActionListener(event -> {
+        myCoursesButton.addActionListener(e -> {
             List<Enrollment> enrollments = Services.getInstance().getEnrollments();
             Student student = Services.getInstance().getStudent();
             List<Course> courses = Services.getInstance().getCourses();
@@ -355,7 +355,7 @@ add(contentPanel, BorderLayout.CENTER);
         viewAllCoursesButton.setPreferredSize(new Dimension(250, 40));
         viewAllCoursesButton.setMaximumSize(new Dimension(250, 40));
 
-        viewAllCoursesButton.addActionListener(event -> {
+        viewAllCoursesButton.addActionListener(e -> {
             new ViewAllCourses().setVisible(true);
         });
         viewAllCoursesButton.setOpaque(false);
@@ -502,7 +502,7 @@ add(contentPanel, BorderLayout.CENTER);
         buttonPanel.add(Box.createRigidArea(new Dimension(10, 0)));
 
         JButton viewButton = new JButton("View/Edit");
-        viewButton.addActionListener(event -> {
+        viewButton.addActionListener(e -> {
             if (selectedRow.apply(dataTable) == -1) {
                 JOptionPane.showMessageDialog(null, "Please select a student to view/edit");
                 return;
@@ -981,7 +981,7 @@ add(contentPanel, BorderLayout.CENTER);
         viewALlResultsButton.setPreferredSize(new Dimension(250, 40));
         viewALlResultsButton.setMaximumSize(new Dimension(250, 40));
 
-        viewALlResultsButton.addActionListener(_ -> {
+        viewALlResultsButton.addActionListener(e -> {
             new ViewAllResults().setVisible(true);
         });
         viewALlResultsButton.setOpaque(false);
