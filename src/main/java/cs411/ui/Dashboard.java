4bd91c18d019dd9563ef22fb83a716516fb5fcd9
Dilpat Rai <67273086+DilpatRai@users.noreply.github.com>
2025-03-19 11:41:34 +0500

    Update Dashboard.java

--- src/main/java/cs411/ui/Dashboard.java
+++ src/main/java/cs411/ui/Dashboard.java
@@ -69,16 +69,15 @@ public class Dashboard extends JFrame {
         contentPanel = new JPanel(cardLayout);
         contentPanel.setBackground(Config.PRIMARY_COLOR);
 
-        tabs.forEach((tab, _) -> {
-            switch (tab) {
-                case "Courses" -> contentPanel.add(createCoursesTab(), tab);
-                case "Students" -> contentPanel.add(createStudentsTab(), tab);
-                case "Enrollments" -> contentPanel.add(Services.getInstance().isAdmin() ? createEnrollmentsTabForAdmin() : createEnrollmentsTabForStudent(), tab);
-                case "Results" -> contentPanel.add(createResultsTab(), tab);
-                default -> System.out.println("Invalid tab: " + tab);
-
-            }
-        });
+        tabs.forEach((tab, index) -> {
+    switch (tab) {
+        case "Courses" -> contentPanel.add(createCoursesTab(), tab);
+        case "Students" -> contentPanel.add(createStudentsTab(), tab);
+        case "Enrollments" -> contentPanel.add(Services.getInstance().isAdmin() ? createEnrollmentsTabForAdmin() : createEnrollmentsTabForStudent(), tab);
+        case "Results" -> contentPanel.add(createResultsTab(), tab);
+        default -> System.out.println("Invalid tab: " + tab);
+    }
+});
         add(contentPanel, BorderLayout.CENTER);
 
 
@@ -91,7 +90,7 @@ public class Dashboard extends JFrame {
         JButton profileButton = new JButton("Profile");
         profileButton.setAlignmentX(Component.CENTER_ALIGNMENT);
         profileButton.setMaximumSize(new Dimension(Integer.MAX_VALUE, 50));
-        profileButton.addActionListener(_ -> openProfile());
+        profileButton.addActionListener(e -> openProfile());
         profileButton.setOpaque(false);
         profileButton.setContentAreaFilled(false);
         profileButton.setBorderPainted(true);
@@ -261,7 +260,7 @@ public class Dashboard extends JFrame {
         JButton updateButton = new JButton("Update");
         updateButton.setAlignmentX(Component.CENTER_ALIGNMENT);
         updateButton.setForeground(Config.PRIMARY_COLOR);
-        updateButton.addActionListener(_ -> {
+        updateButton.addActionListener(e -> {
             if (passwordField.getText().trim().length() < 8) {
                 JOptionPane.showMessageDialog(null, "Password must be at least 8 characters long");
                 return;
@@ -327,7 +326,7 @@ public class Dashboard extends JFrame {
         myCoursesButton.setMinimumSize(new Dimension(250, 40));
         myCoursesButton.setPreferredSize(new Dimension(250, 40));
         myCoursesButton.setMaximumSize(new Dimension(250, 40));
-        myCoursesButton.addActionListener(_ -> {
+        myCoursesButton.addActionListener(e -> {
             List<Enrollment> enrollments = Services.getInstance().getEnrollments();
             Student student = Services.getInstance().getStudent();
             List<Course> courses = Services.getInstance().getCourses();
@@ -356,7 +355,7 @@ public class Dashboard extends JFrame {
         viewAllCoursesButton.setPreferredSize(new Dimension(250, 40));
         viewAllCoursesButton.setMaximumSize(new Dimension(250, 40));
 
-        viewAllCoursesButton.addActionListener(_ -> {
+        viewAllCoursesButton.addActionListener(e -> {
             new ViewAllCourses().setVisible(true);
         });
         viewAllCoursesButton.setOpaque(false);
@@ -503,7 +502,7 @@ public class Dashboard extends JFrame {
         buttonPanel.add(Box.createRigidArea(new Dimension(10, 0)));
 
         JButton viewButton = new JButton("View/Edit");
-        viewButton.addActionListener(_ -> {
+        viewButton.addActionListener(e -> {
             if (selectedRow.apply(dataTable) == -1) {
                 JOptionPane.showMessageDialog(null, "Please select a student to view/edit");
                 return;
@@ -982,7 +981,7 @@ public class Dashboard extends JFrame {
         viewALlResultsButton.setPreferredSize(new Dimension(250, 40));
         viewALlResultsButton.setMaximumSize(new Dimension(250, 40));
 
-        viewALlResultsButton.addActionListener(_ -> {
+        viewALlResultsButton.addActionListener(e -> {
             new ViewAllResults().setVisible(true);
         });
         viewALlResultsButton.setOpaque(false);
